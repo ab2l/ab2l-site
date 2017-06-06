7627ae5e7faa0b6d2f047b4da5f3676e9ff13642
Bruno Cabral <brataodream@gmail.com>
2017-06-06 17:34:57 -0300

    Mostrar aleatorio

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -24,9 +24,24 @@ export default {
   created() {
     axios.get('/static/companies/companies.json')
       .then(({ data }) => {
-        this.companies = data.companies;
+        this.companies = this.shuffleArray(data.companies);
       });
   },
+  methods: {
+    /**
+   * Randomize array element order in-place.
+   * Using Durstenfeld shuffle algorithm.
+   */
+  shuffleArray(array) {
+      for (var i = array.length - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * (i + 1));
+          var temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+      }
+    return array;
+  },
+  }
 };
 </script>
 
