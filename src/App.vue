28574bc026656ceb856d7db750848fcd5c5f0293
Ramon Sena <ramonsenadev@gmail.com>
2017-06-06 16:41:14 -0300

    Versão inicial

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <header class="Header">
+      <img class="Header__logo" src="./assets/logo.png">
+    </header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+  html, body {
+    min-height: 100%;
+    background: #f9f9f9;
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    padding-bottom: 48px;
+  }
+  .Header {
+    background: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 200px;
+    user-select: none;
+  }
+  .Header__logo {
+    max-width: 236px;
+    width: 100%;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <section>
+    <div class="CompanieList">
+      <a
+        v-for="companie in companies"
+        :href="companie.website"
+        class="CompanieList__item"
+        target="_blank"
+      >
+        <img class="CompanieList__image" :src="`/static/companies/images/${companie.logo}`" :alt="companie.name">
+      </a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+      companies: [],
+    };
+  },
+  created() {
+    axios.get('/static/companies/companies.json')
+      .then(({ data }) => {
+        this.companies = data.companies;
+      });
+  },
+};
+</script>
+
+
+<style>
+  .CompanieList {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding-top: 24px;
+  }
+
+  .CompanieList__item:hover {
+    transform: scale(1.1);
+    box-shadow: 0 12px 48px rgba(0, 0, 0, 0.1);
+    z-index: 2;
+  }
+  .CompanieList__item {
+    width: 300px;
+    height: 300px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #fff;
+    margin: 6px;
+    transition: all 0.3s;
+  }
+
+  .CompanieList__image {
+    max-width: 200px;
+    max-height: 140px;
+  }
+</style>
